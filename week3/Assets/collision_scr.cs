85f8bbb1997ad768ed41447d405f4341b3c73f37
ParkYunri <yoyoyo066@naver.com>
2022-04-27 15:03:41 +0900

    upload 4

---
+++ week3/Assets/collision_scr.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collision_scr : MonoBehaviour
+{
+    public float power = 500.0f;
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Cube")
+            this.GetComponent<Rigidbody>().AddForce(new Vector3(0, 1, 1) * power);
+    }
+}
