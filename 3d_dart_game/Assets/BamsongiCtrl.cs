5d1c3a33e7aa2af1a3cebb8bd9974e038fc05310
ParkYunri <yoyoyo066@naver.com>
2022-04-27 14:34:21 +0900

    upload 2

---
+++ 3d_dart_game/Assets/BamsongiCtrl.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BamsongiCtrl : MonoBehaviour
+{
+    float timer = 0.0f;
+    int score = 0;
+    bool is_changed = false;
+    private BamsongiGenerator bamsongi;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bamsongi = GameObject.Find("bamsongi_spawn").GetComponent<BamsongiGenerator>();
+        Debug.Log(bamsongi.wind);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        GetComponent<Rigidbody>().AddForce(bamsongi.wind);
+        if (timer >= 2 && !is_changed)
+        {
+            bamsongi.wind = new Vector3(Random.Range(-4.0f, 4.0f), Random.Range(-1.0f, 4.0f), 0.0f);
+            is_changed = true;
+            
+            //Debug.Log(bamsongi.wind);
+            //Debug.Log(is_changed);
+        }
+        //Debug.Log(wind);
+    }
+
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        bamsongi.wind = new Vector3(Random.Range(-4.0f, 4.0f), Random.Range(-1.0f, 4.0f), 0.0f);
+        is_changed = true;
+        Debug.Log(bamsongi.wind);
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<ParticleSystem>().Play();
+        Vector3 collided_position = transform.position;
+        float distance = collided_position.x * collided_position.x +
+            (collided_position.y - 6.5f) * (collided_position.y - 6.5f);
+        distance = Mathf.Sqrt(distance);
+
+
+        if (distance <= 0.4f)
+            score = 100;
+        else if (distance <= 0.8f)
+            score = 90;
+        else if (distance <= 1.2f)
+            score = 70;
+        else if (distance <= 1.6f)
+            score = 50;
+        else if (distance <= 2.0f)
+            score = 30;
+        else
+            score = 0;
+
+        bamsongi.score += score;
+    }
+}
---
+++ 3d_dart_game/Assets/BamsongiGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BamsongiGenerator : MonoBehaviour
+{
+    public GameObject bamsongi_prefab;
+    public Vector3 wind;
+    public int score = 0;
+    public int bam_count = 0;
+    public GameObject pre_bam;
+    public GameObject text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        wind = new Vector3(Random.Range(-4.0f, 4.0f), Random.Range(-1.0f, 4.0f), 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && bam_count < 5)
+        {
+            bam_count++;
+            Destroy(pre_bam);
+
+            GameObject bamsongi = Instantiate(bamsongi_prefab) as GameObject;
+            Ray screen_ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 shooting_ray = screen_ray.direction;
+            bamsongi.GetComponent<BamsongiCtrl>().Shoot(shooting_ray * 1000);
+            if (bam_count >= 5)
+            {
+                text.SetActive(true);
+            }
+            
+            pre_bam = bamsongi;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            score = 0;
+            bam_count = 0;
+            text.SetActive(false);
+            Destroy(pre_bam);
+        }
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(81, Screen.height - 20, 128, 32), wind.ToString());
+        GUI.Label(new Rect(81, Screen.height - 40, 128, 32), bam_count +" : "+score.ToString());
+    }
+}
