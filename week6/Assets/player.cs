2c31cb358c9b1e08f00401031c90b0c0f0fb39f2
ParkYunri <yoyoyo066@naver.com>
2022-04-27 15:04:48 +0900

    upload 6

---
+++ week6/Assets/player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class player : MonoBehaviour
+{
+    public int jump_power;
+    private float gameTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        jump_power = Random.Range(5, 8);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(0, jump_power, 0);
+        }
+        gameTime += Time.deltaTime;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        SceneManager.LoadScene("main_scene");
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(81, Screen.height - 40, 128, 32), jump_power.ToString());
+        GUI.Label(new Rect(Screen.width / 2 - 30, 80, 100, 20), gameTime.ToString());
+    }
+}
---
+++ week6/Assets/spawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn : MonoBehaviour
+{
+    public GameObject pf_wall;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            float interval = Random.Range(1.0f, 2.0f);
+            float randomY = Random.Range(-3.0f, 4.0f);
+            transform.position = new Vector3(0.0f, randomY, 0.0f);
+
+            Instantiate(pf_wall, transform.position, transform.rotation);
+            yield return new WaitForSeconds(interval);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
